cfadc2fdbd221348e43c9bc4d0e739b9e221e3ca
LucasLoopsT <128094146+LucasLoopsT@users.noreply.github.com>
2024-07-30 16:42:09 -0300

    Created Global Theme

---
+++ src/Styles/global.jsx
@@ -0,0 +1,30 @@
+import { createGlobalStyle } from "styled-components";
+
+export default createGlobalStyle`
+    *{
+        box-sizing: border-box;
+        margin: 0;
+        padding: 0;
+    }
+
+    body{
+        background-color: ${({theme}) => theme.COLORS.bg};
+        color: ${({theme}) => theme.COLORS.WHITE};
+        -webkit-font-smoothing: antileased;
+    }
+
+    body, input, button, textarea{
+        font-family: "Montserrat", sans-serif;
+        font-size: 16px;
+        outline: none;
+    }
+
+    button, a{
+        cursor: pointer;
+        transition: filter 0.2s;
+    }
+
+    button:hover, a:hover{
+        filter: brightness(0.9);
+    }
+`;
---
+++ src/Styles/theme.jsx
@@ -0,0 +1,14 @@
+export default {
+    COLORS:{
+        bg:"#202020",  
+        bg_light:"#3b3b3b",  
+        bg_dark: "#101010",  
+        bg_dark_70: "#101010b0",  
+        
+        1:"#12FF65",  
+        2:"#AFFFCA",  
+        3:"#fff",  
+        4:"#fff",  
+        5:"#fff",  
+    }
+}
