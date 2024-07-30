e1d54eeae1a3cffe8b464a984793541cf60218c4
LucasLoopsT <128094146+LucasLoopsT@users.noreply.github.com>
2024-07-30 16:44:42 -0300

    Config index/main - GlobalStyles

--- src/main.jsx
+++ src/main.jsx
@@ -1,21 +1,24 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+// Styled-components
+import { ThemeProvider } from 'styled-components';
+import theme from './styles/theme.jsx';
+import GlobalStyle from './styles/global.jsx';
+//Pages
 import Header from './pages/Header.jsx'
 import Hello from './pages/Hello.jsx'
 import Techs from './pages/Techs.jsx'
 import Projects from './pages/Projects.jsx'
 import './index.css'
 
-ReactDOM.createRoot(document.querySelector('header')).render(
-  <React.StrictMode>
-    <Header />
-  </React.StrictMode>,
-)
-
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Hello/>
-    <Techs/>
-    <Projects />
+    <ThemeProvider theme={theme}>
+      <GlobalStyle />
+      <Header />
+      <Hello />
+      <Techs />
+      <Projects />
+    </ThemeProvider>
   </React.StrictMode>,
 )
