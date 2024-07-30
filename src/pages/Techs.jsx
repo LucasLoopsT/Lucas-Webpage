d35605d96a6c977f28ce90caf9ff930846a02630
LucasLoopsT <128094146+LucasLoopsT@users.noreply.github.com>
2024-07-29 21:55:12 -0300

    Tech Section Created

--- src/pages/Techs.jsx
+++ src/pages/Techs.jsx
@@ -6,7 +6,31 @@ function Techs
   const [count, setCount] = useState(0)
   return (
     <>
-      <div></div>
+      <section id="Techs">
+        <ul id="Icons">
+          <li>
+            <img src="https://skillicons.dev/icons?i=html" alt="" />
+          </li>
+          <li>
+            <img src="https://skillicons.dev/icons?i=css" alt="" />
+          </li>
+          <li>
+            <img src="https://skillicons.dev/icons?i=js" alt="" />
+          </li>
+          <li>
+            <img src="https://skillicons.dev/icons?i=react" alt="" />
+          </li>
+          <li>
+            <img src="https://skillicons.dev/icons?i=nodejs" alt="" />
+          </li>
+          <li>
+            <img src="https://skillicons.dev/icons?i=mysql" alt="" />
+          </li>
+          <li>
+            <img src="https://skillicons.dev/icons?i=mongo" alt="" />
+          </li>
+        </ul>
+      </section>
     </>
   )
 }
