9488cc80417cf4ca678a5dce4583feb03b99a7f0
LucasLoopsT <128094146+LucasLoopsT@users.noreply.github.com>
2024-07-30 16:46:10 -0300

    More Techs

--- src/pages/Techs.jsx
+++ src/pages/Techs.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-import '../Styles/Techs.css'
+import '../styles/Techs.css'
 
 function Techs
   () {
@@ -8,27 +8,16 @@ function Techs
     <>
       <section id="Techs">
         <ul id="Icons">
-          <li>
-            <img src="https://skillicons.dev/icons?i=html" alt="" />
-          </li>
-          <li>
-            <img src="https://skillicons.dev/icons?i=css" alt="" />
-          </li>
-          <li>
-            <img src="https://skillicons.dev/icons?i=js" alt="" />
-          </li>
-          <li>
-            <img src="https://skillicons.dev/icons?i=react" alt="" />
-          </li>
-          <li>
-            <img src="https://skillicons.dev/icons?i=nodejs" alt="" />
-          </li>
-          <li>
-            <img src="https://skillicons.dev/icons?i=mysql" alt="" />
-          </li>
-          <li>
-            <img src="https://skillicons.dev/icons?i=mongo" alt="" />
-          </li>
+          <li><img src="https://skillicons.dev/icons?i=html" alt="" /></li>
+          <li><img src="https://skillicons.dev/icons?i=css" alt="" /></li>
+          <li><img src="https://skillicons.dev/icons?i=js" alt="" /></li>
+          <li><img src="https://skillicons.dev/icons?i=react" alt="" /></li>
+          <li><img src="https://skillicons.dev/icons?i=vite" alt="" /></li>
+          <li><img src="https://skillicons.dev/icons?i=styledcomponents" alt="" /></li>
+          <li><img src="https://skillicons.dev/icons?i=nodejs" alt="" /></li>
+          <li><img src="https://skillicons.dev/icons?i=express" alt="" /></li>
+          <li><img src="https://skillicons.dev/icons?i=mysql" alt="" /></li>
+          <li><img src="https://skillicons.dev/icons?i=mongo" alt="" /></li>
         </ul>
       </section>
     </>
