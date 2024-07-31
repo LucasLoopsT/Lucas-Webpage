dd16faf7e6c2f4cfc961bb60778fb43205d4777b
LucasLoopsT <128094146+LucasLoopsT@users.noreply.github.com>
2024-07-31 18:55:43 -0300

    Update: Old Page Components Removed

--- src/main.jsx
+++ src/main.jsx
@@ -5,19 +5,13 @@ import { ThemeProvider } from 'styled-components';
 import theme from './styles/theme.jsx';
 import GlobalStyle from './styles/global.jsx';
 //Pages
-import Header from './pages/Header/index.jsx'
-import Hello from './pages/Hello/index.jsx'
-import Techs from './pages/Techs/index.jsx'
-import Projects from './pages/Projects/index.jsx'
+import Home from './pages/Home/index.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <GlobalStyle />
-      <Header />
-      <Hello />
-      <Techs />
-      <Projects />
+      <Home />
     </ThemeProvider>
   </React.StrictMode>,
 )
