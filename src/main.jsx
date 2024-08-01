f3c62ba2f12ca483130589527e208dccf00a8938
LucasLoopsT <128094146+LucasLoopsT@users.noreply.github.com>
2024-08-01 18:15:28 -0300

    Create: Import Swiper

--- src/main.jsx
+++ src/main.jsx
@@ -6,6 +6,14 @@ import theme from './styles/theme.jsx';
 import GlobalStyle from './styles/global.jsx';
 //Pages
 import Home from './pages/Home/index.jsx';
+//Swiper
+import { register } from 'swiper/element/bundle';
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import 'swiper/css/scrollbar';
+import 'swiper/css/effect-coverflow';
+register();
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
