e50d9be6e4af4647fca5d03fd03c1b5aacf18644
LucasLoopsT <128094146+LucasLoopsT@users.noreply.github.com>
2024-09-07 20:27:52 -0300

    chore: configure backend structure

--- Backend/index.js
+++ Backend/index.js
@@ -1,8 +1,14 @@
-const express = require("express");
+import express from "express";
+import routes from "./src/routes/index.js";
+import connectDatabase from "./src/database/db.js";
+import dotenv from "dotenv";
+dotenv.config();
+
+const port = process.env.PORT || 3000;
 const app = express();
+connectDatabase();
 
-app.get("/", (req, res) => {
-    res.send("Running");
-});
+app.use(express.json());
+app.use(routes);
 
-app.listen(3000);
+app.listen(port, () => console.log(`Servidor rodando na porta ${port}`));
