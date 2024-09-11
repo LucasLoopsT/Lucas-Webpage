bcb82dd4674732752d9063dc8ba32defb4e48269
LucasLoopsT <128094146+LucasLoopsT@users.noreply.github.com>
2024-09-10 23:58:04 -0300

    Fixed imports in global.middlewares

--- Backend/src/middlewares/global.middlewares.js
+++ Backend/src/middlewares/global.middlewares.js
@@ -1,6 +1,6 @@
 import mongoose from "mongoose"
 import ProjectServices from "../services/project.services.js"
-import UserServices from "../services/project.services.js"
+import UserServices from "../services/user.services.js"
 const proServices = new ProjectServices();
 const userServices = new UserServices();
 
@@ -32,7 +32,7 @@ export const validUser = async (req, res, next) => {
 export const validProject = async (req, res, next) => {
     const id = req.params.id;
     
-    const project = await proServices.findById(id)
+    const project = await proServices.findById(id);
 
     if(!project){
         return res.status(400).send({message:"Project not found."}) 
