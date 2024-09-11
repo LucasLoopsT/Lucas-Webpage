a2c063636030a4899faa6861d4db0c53290686e3
LucasLoopsT <128094146+LucasLoopsT@users.noreply.github.com>
2024-09-10 23:58:14 -0300

    Added auth.middleware to project.routes

--- Backend/src/routes/project.routes.js
+++ Backend/src/routes/project.routes.js
@@ -1,12 +1,14 @@
 import {Router} from 'express'
 const projectRouter = Router();
-import {Controller} from "../controllers/project.crontroller.js";
+import {Controller} from "../controllers/project.controller.js";
 const controller = new Controller();
 import {validId, validProject} from "../middlewares/global.middlewares.js";
+import {authMiddleware} from "../middlewares/auth.middlewares.js";
 
-projectRouter.post("/", controller.create);
-projectRouter.get("/", controller.findAll);
-projectRouter.patch("/:id", validId, validProject, controller.update);
-projectRouter.delete("/", controller.delete);
+projectRouter.post("/", authMiddleware, controller.create);
+projectRouter.get("/", authMiddleware, controller.findAll);
+projectRouter.get("/:id", authMiddleware, controller.findById);
+projectRouter.patch("/:id", authMiddleware, validId, validProject, controller.update);
+projectRouter.delete("/:id", authMiddleware, validId, validProject, controller.delete);
 
 export default projectRouter;
