c99f60237bffd63e2e166e528ec9a09251636871
LucasLoopsT <128094146+LucasLoopsT@users.noreply.github.com>
2024-09-10 23:58:21 -0300

    Added routes for findById and delete in user.routes

--- Backend/src/routes/user.routes.js
+++ Backend/src/routes/user.routes.js
@@ -1,11 +1,13 @@
 import {Router} from 'express'
-const userRouter = Router();;
+const userRouter = Router();
 import {Controller} from "../controllers/user.controller.js";
 const controller = new Controller();
 import {validId, validUser} from "../middlewares/global.middlewares.js";
 
 userRouter.post("/", controller.create);
 userRouter.get("/", controller.findAll);
+userRouter.get("/:id", controller.findById);
 userRouter.patch("/:id", validId, validUser, controller.update);
+userRouter.delete("/:id", validId, validUser, controller.delete);
 
 export default userRouter;
