473b75614cf75266a344dbfffacd5c6c75c33d0f
LucasLoopsT <128094146+LucasLoopsT@users.noreply.github.com>
2024-09-10 23:58:34 -0300

    Added delete function in project.services

--- Backend/src/services/project.services.js
+++ Backend/src/services/project.services.js
@@ -5,7 +5,7 @@ class Services{
     findAll = () => Project.find();
     findById = (id) => Project.findById(id);
     update = (id, name, preview, shortDescription, description, techs, link_git, link_deploy) => Project.findByIdAndUpdate({_id: id}, {name, preview, shortDescription, description, techs, link_git, link_deploy});
-    delete = (id) => Project.deleteOne(id);
+    delete = (id) => Project.deleteOne({_id: id});
 }
 
 export default Services;
