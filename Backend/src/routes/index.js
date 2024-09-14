92a6f0fe18e51c13587be2fd58da115d6b1ccc09
LucasLoopsT <128094146+LucasLoopsT@users.noreply.github.com>
2024-09-14 14:49:48 -0300

    feat: add new route for Swagger documentation

--- Backend/src/routes/index.js
+++ Backend/src/routes/index.js
@@ -3,9 +3,11 @@ const routes = express.Router();
 import userRouter from "../routes/user.routes.js"
 import authRouter from "../routes/auth.routes.js"
 import projectRouter from "../routes/project.routes.js"
+import swaggerRouter from "../routes/swagger.routes.js"
 
 routes.use("/user", userRouter);
 routes.use("/auth", authRouter);
 routes.use("/project", projectRouter);
+routes.use("/doc", swaggerRouter);
 
 export default routes;
