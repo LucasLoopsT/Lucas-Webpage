680b3bececfe0974d86b4a7bb2e746f1ca4b85d8
LucasLoopsT <128094146+LucasLoopsT@users.noreply.github.com>
2024-09-14 14:50:22 -0300

    feat: add auth middleware to project and user routes

--- Backend/src/routes/project.routes.js
+++ Backend/src/routes/project.routes.js
@@ -6,9 +6,9 @@ import {validId, validProject} from "../middlewares/global.middlewares.js";
 import {authMiddleware} from "../middlewares/auth.middlewares.js";
 
 projectRouter.post("/", authMiddleware, controller.create);
-projectRouter.get("/", authMiddleware, controller.findAll);
+projectRouter.get("/", controller.findAll);
 projectRouter.get("/:id", authMiddleware, controller.findById);
 projectRouter.patch("/:id", authMiddleware, validId, validProject, controller.update);
-projectRouter.delete("/:id", authMiddleware, validId, validProject, controller.delete);
+projectRouter.delete("/delete/:id", authMiddleware, validId, validProject, controller.delete);
 
 export default projectRouter;
--- Backend/src/routes/user.routes.js
+++ Backend/src/routes/user.routes.js
@@ -3,11 +3,12 @@ const userRouter = Router();
 import {Controller} from "../controllers/user.controller.js";
 const controller = new Controller();
 import {validId, validUser} from "../middlewares/global.middlewares.js";
+import {authMiddleware} from "../middlewares/auth.middlewares.js";
 
-userRouter.post("/", controller.create);
+userRouter.post("/", authMiddleware, controller.create);
 userRouter.get("/", controller.findAll);
-userRouter.get("/:id", controller.findById);
-userRouter.patch("/:id", validId, validUser, controller.update);
-userRouter.delete("/:id", validId, validUser, controller.delete);
+userRouter.get("/:id", authMiddleware, controller.findById);
+userRouter.patch("/:id", authMiddleware, validId, validUser, controller.update);
+userRouter.delete("/delete/:id", authMiddleware, validId, validUser, controller.delete);
 
 export default userRouter;
