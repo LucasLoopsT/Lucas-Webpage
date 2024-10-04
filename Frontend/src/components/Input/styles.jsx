4e81706b3d8bb8f6d1a76608fb36f1341cd8819b
LucasLoopsT <128094146+LucasLoopsT@users.noreply.github.com>
2024-10-04 11:59:36 -0300

    style: input webkit style change

--- Frontend/src/components/Input/styles.jsx
+++ Frontend/src/components/Input/styles.jsx
@@ -1,16 +1,16 @@
 import styled from 'styled-components';
 
 export const Container = styled.div`
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    gap: 5px;
-    width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 5px;
+  width: 100%;
 
-    label{
-        width: 70%;
-        color: white;
-    }
+  label{
+      width: 70%;
+      color: white;
+  }
 `
 
 export const InputWrapper = styled.div`
@@ -27,9 +27,19 @@ export const InputWrapper = styled.div`
     border: none;
     outline: none;
     border-radius: 10px;
-    background-color: ${({theme}) => theme.COLORS.bg};
+    background-color: ${({ theme }) => theme.COLORS.bg}!important;
     color: white;
-    }
+  }
+
+  input:-webkit-autofill,
+  input:-webkit-autofill:hover,
+  input:-webkit-autofill:focus,
+  input:-webkit-autofill:active {
+    -webkit-box-shadow: 0 0 0px 1000px #202020 inset !important;
+    -webkit-text-fill-color: white !important;
+    box-shadow: 0 0 0px 1000px #202020 inset !important;
+    font-family: 'Montserrat', sans-serif; 
+  }
 `;
 
 export const IconWrapper = styled.div`
@@ -41,7 +51,7 @@ export const IconWrapper = styled.div`
   height: 100%;
 
   svg {
-    font-size: 20px; /* Tamanho do ícone */
-    color: ${({theme}) => theme.COLORS.color_2};
+    font-size: 20px;
+    color: ${({ theme }) => theme.COLORS.color_2};
   }
 `;
